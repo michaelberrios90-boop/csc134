4dde24e96e40595d56418e7b100e0cc64d34dfce
michael-berrios <michaelberrios90@gmail.com>
2025-09-21 00:35:50 -0400

    M2T1_Berrios_Michael.cpp
    started M2T1

---
+++ M2T1_Berrios_Michael.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+using namespace std;
+
+int main () {
+  // Set up variables 
+  string first_name, last_name, full_name; // hold customer's name
+  string product = "mints"; // holds product name
+  double cost_each = 0.99
+  double total_cost; 
+
+  // Greet the customer
+  cout << "Welcome to our" << product << " store." << endl;
+  cout << "What's your first name?";
+  cin >> first_name;
+  cout << "What's your last name?";
+  cin >> last_name;
+  full_name = first_name + " " + last_name;
+  cout << "Nice to meet you " << full_name << endl;
+  
+  // Ask how much they'd like to purchase
+
+  // Calculate total price
+  
+  // Give the result
+  
+  
+
+
+
+
+
+
+
+
+
+
+  return 0; // Check for errors. 0 = Good
+}
