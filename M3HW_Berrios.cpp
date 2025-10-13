98d800c1642fc8d79776c4d9b3c39ddd121a6599
michael-berrios <michaelberrios90@gmail.com>
2025-10-13 16:09:50 -0400

    Update M3HW_Berrios.cpp
    Finished Question 1 code. Three more to go!

--- M3HW_Berrios.cpp
+++ M3HW_Berrios.cpp
@@ -1,6 +1,6 @@
 /*
 CSC 134 
-M3HW- Gold Tier
+M3HW Question 1- Gold Tier
 michaelberrios90
 10/13/2025
 */
@@ -9,10 +9,27 @@ michaelberrios90
 #include <iomanip>
 using namespace std; 
 
-int main(){
+int main()
+{
 
+    // Declare variables
+    string responseAnswer;
 
+    // Display opening greeting
+    cout << "Hello, I'm a C++ program!" << endl;
 
+    // Ask user if they like it
+    cout << "Do you like me? Please type yes or no. " << endl;
+    cin >> responseAnswer;
 
-  
+    // Set up responseAnswer if-else conditional structure
+    if (responseAnswer == "yes") {
+        cout << "That's great! I'm sure we'll get along." << endl; }
+    else if (responseAnswer == "no") {
+        cout << "Well, maybe you'll learn to like me later." << endl; }
+    else {
+        cout << "If you're not sure...that's OK." << endl;
+    }
+
+    return 0;  
 }
