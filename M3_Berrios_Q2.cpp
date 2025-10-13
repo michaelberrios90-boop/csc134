baa7017cb2499146327271e310e37b55912eb10a
michael-berrios <michaelberrios90@gmail.com>
2025-10-13 16:24:18 -0400

    Create M3_Berrios_Q2.cpp
    Created M3HW Q2 file with header and std code starter headings

---
+++ M3_Berrios_Q2.cpp
@@ -0,0 +1,35 @@
+/*
+CSC 134
+M3HW Question 2- Gold Tier
+michaelberrios90
+10/13/2025
+*/
+
+# include <iostream>
+# include <iomanip>
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+}
