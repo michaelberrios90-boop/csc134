5a6f133eb726dd4c46aa3a625028d1e3d38b849b
michael-berrios <michaelberrios90@gmail.com>
2025-10-23 23:20:18 -0400

    Final version

--- M4T1_Berrios.cpp
+++ M4T1_Berrios.cpp
@@ -5,13 +5,37 @@ michaelberrios90
 10/23/2025
 */
 
+// program 5-3
 #include <iostream>
 #include <iomanip>
 using namespace std; 
 
 int main () {
+    int number = 5;
 
+    int i = 0;
+    while (i < number) {
+        cout << "Hello" << endl;
+        // i = i + 1
+        i++; // short hand for i = i +1
+    }
+    cout << "That's all!" << endl;
+    
+// program 5-6
+    const int MIN_NUMBER = 1;
+    const int MAX_NUMBER = 10;
 
+    int num = MIN_NUMBER;
+    // print a table header
+    cout << "Number\tNumber Squared" << endl;
 
-return 0;
+    while (num <= MAX_NUMBER) {
+        cout << num << "\t\t" << num * num << endl;
+        // if we don't increase num here,
+        // it will be an infinite loop, which is a big no no.
+        num ++;
+    }
+    
+    return 0;
 }
+
