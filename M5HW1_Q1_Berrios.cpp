ffbdee2afb6ec6689fd92a4717fc109c4dd6c9f5
michael-berrios <michaelberrios90@gmail.com>
2025-11-17 16:10:02 -0500

    Create M5HW1_Q1_Berrios.cpp for rainfall calculation
    Initial implementation of a program to calculate average rainfall for three months.

---
+++ M5HW1_Q1_Berrios.cpp
@@ -0,0 +1,32 @@
+// CSC 134
+// michaelberrios90
+// M5HW1 - Gold Tier
+// 11/17/2025
+// Program is to calculate the average rainfall for three months.
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+//Declare variables 
+  string month [3];
+  int rainfallAmount, num = 3;
+  double averageRainfall, totalRainfall= 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
